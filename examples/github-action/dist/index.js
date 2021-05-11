e972f4970e8e5513b762fa86f8ea58b06397c333
Umidbek Karimov <4734297+umidbekk@users.noreply.github.com>
2021-05-11 22:35:55 +0500

    WIP

--- examples/github-action/dist/index.js
+++ examples/github-action/dist/index.js
@@ -1,24 +1,17 @@
 var __defProp = Object.defineProperty;
-var __name = (target, value) =>
-  __defProp(target, "name", { value, configurable: true });
-var __commonJS = (cb, mod) =>
-  function __require() {
-    return (
-      mod || (0, cb[Object.keys(cb)[0]])((mod = { exports: {} }).exports, mod),
-      mod.exports
-    );
-  };
+var __name = (target, value) => __defProp(target, "name", {value, configurable: true});
+var __commonJS = (cb, mod) => function __require() {
+  return mod || (0, cb[Object.keys(cb)[0]])((mod = {exports: {}}).exports, mod), mod.exports;
+};
 
 // ../../node_modules/isobject/index.js
 var require_isobject = __commonJS({
   "../../node_modules/isobject/index.js"(exports2, module2) {
     "use strict";
     module2.exports = /* @__PURE__ */ __name(function isObject2(val) {
-      return (
-        val != null && typeof val === "object" && Array.isArray(val) === false
-      );
+      return val != null && typeof val === "object" && Array.isArray(val) === false;
     }, "isObject");
-  },
+  }
 });
 
 // src/index.ts
--- examples/node-version/dist/main.js
+++ examples/node-version/dist/main.js
@@ -1,24 +1,21 @@
 var __defProp = Object.defineProperty;
-var __markAsModule = (target) =>
-  __defProp(target, "__esModule", { value: true });
-var __name = (target, value) =>
-  __defProp(target, "name", { value, configurable: true });
+var __markAsModule = (target) => __defProp(target, "__esModule", {value: true});
+var __name = (target, value) => __defProp(target, "name", {value, configurable: true});
 var __export = (target, all) => {
   for (var name in all)
-    __defProp(target, name, { get: all[name], enumerable: true });
+    __defProp(target, name, {get: all[name], enumerable: true});
 };
 
 // src/index.ts
 __markAsModule(exports);
 __export(exports, {
-  isAdmin: () => isAdmin,
+  isAdmin: () => isAdmin
 });
 function isAdmin(user) {
   return user?.roles?.admin ?? false;
 }
 __name(isAdmin, "isAdmin");
 // Annotate the CommonJS export names for ESM import in node:
-0 &&
-  (module.exports = {
-    isAdmin,
-  });
+0 && (module.exports = {
+  isAdmin
+});
