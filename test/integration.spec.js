0baed89119906cf91a6adabeb9a98431cc8e9a02
Umidbek Karimov <4734297+umidbekk@users.noreply.github.com>
2021-05-11 20:05:21 +0500

    test: Require `bin` file to collect coverage report.

--- test/integration.spec.js
+++ test/integration.spec.js
@@ -16,10 +16,31 @@ expect.addSnapshotSerializer({
     return typeof value == "string" && snapshots.has(value);
   },
   print(value) {
-    return String(value)
-      .trim()
-      .replace(/(\[\d\d:\d\d:\d\d])/g, "[HH:mm:ss]");
+    return String(value);
   },
+});
+
+/** @type {jest.SpyInstance} */
+let log;
+
+function extractLogs() {
+  const message = log.mock.calls
+    .map(([line]) => line.replace(/^(\[\d\d:\d\d:\d\d])/, "[HH:mm:ss]"))
+    .join("\n");
+
+  snapshots.add(message);
+
+  return message;
+}
+
+beforeEach(() => {
+  process.stdout.isTTY = false;
+  log = jest.spyOn(console, "log").mockImplementation();
+});
+
+afterAll(() => {
+  jest.resetModules();
+  jest.restoreAllMocks();
 });
 
 for (const example of fs.readdirSync(EXAMPLES_DIR)) {
@@ -27,24 +48,14 @@ for (const example of fs.readdirSync(EXAMPLES_DIR)) {
   if (!fs.statSync(exampleDir).isDirectory()) continue;
 
   test(example, async () => {
-    const { stderr, stdout } = await execa.node(BIN_PATH, {
-      reject: false,
-      cwd: exampleDir,
-    });
+    jest.spyOn(process, "cwd").mockImplementation(() => exampleDir);
 
-    await execa.node(exampleDir);
+    await require(BIN_PATH);
 
-    snapshots.add(stdout);
+    expect(extractLogs()).toMatchSnapshot();
 
-    expect(stderr).toBe("");
-    expect(stdout).toMatchSnapshot(example);
-
-    const { stdout: gitStatus } = await execa("git", [
-      "status",
-      "--porcelain",
-      path.join(exampleDir, "dist"),
-    ]);
-
-    expect(gitStatus).toBe("");
+    await expect(
+      execa("git", ["status", "--porcelain", path.join(exampleDir, "dist")])
+    ).resolves.toMatchObject({ stdout: "" });
   });
 }
