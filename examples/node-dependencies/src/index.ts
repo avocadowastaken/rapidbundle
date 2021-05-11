a354286e8b3fa749c31b23c664af4e700841006d
Umidbek Karimov <4734297+umidbekk@users.noreply.github.com>
2021-05-11 21:58:38 +0500

    feat: Add GitHub action bundle step.

--- examples/node-dependencies/src/index.ts
+++ examples/node-dependencies/src/index.ts
@@ -1,8 +1,4 @@
 import debug = require('debug');
-import arrify = require('arrify');
 import isObject = require('isobject');
 
-const logger = debug('app');
-export function log(format: string, args: unknown) {
-  logger(format, ...arrify(args))
-}
+export const logger = debug('app');
