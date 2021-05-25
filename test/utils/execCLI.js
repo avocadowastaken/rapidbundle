f49785d6d8ca7935c3798dbf7aa3a15a176cba5c
Umidbek Karimov <4734297+umidbekk@users.noreply.github.com>
2021-05-25 17:44:11 +0500

    chore: Run integration tests in child process.

---
+++ test/utils/execCLI.js
@@ -0,0 +1,43 @@
+"use strict";
+
+const path = require("path");
+const execa = require("execa");
+const stripANSI = require("strip-ansi");
+
+const registerRawSnapshot = require("./registerRawSnapshot");
+
+const ROOT_DIR = path.join(__dirname, "..", "..");
+const BIN = path.join(ROOT_DIR, "bin");
+
+/**
+ * @param {string} input
+ * @returns {string}
+ */
+function cleanupLogs(input) {
+  return input
+    .split("\n")
+    .map((line) =>
+      stripANSI(line)
+        .replace(/^(\[\d\d:\d\d:\d\d])/, "[HH:mm:ss]")
+        .replace(ROOT_DIR, "<rootDir>")
+        .replace(/\\/g, "/")
+    )
+    .join("\n");
+}
+
+/**
+ * @param {string} cwd
+ * @param {string[]} [args]
+ * @returns {Promise<[stdout: string, stderr: string, exitCode: number]>}
+ */
+module.exports = async function execCLI(cwd, args = []) {
+  const result = await execa.node(BIN, args, { cwd, reject: false });
+
+  const stdout = cleanupLogs(result.stdout);
+  const stderr = cleanupLogs(result.stderr);
+
+  registerRawSnapshot(stdout);
+  registerRawSnapshot(stderr);
+
+  return [stdout, stderr, result.exitCode];
+};
