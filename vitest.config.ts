cb103dc604558ec77d32618b71ab4762b3c4fbef
Umidbek Karimov <4734297+avocadowastaken@users.noreply.github.com>
2023-02-13 23:28:10 +0200

    test: Track coverage.

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,18 +1,19 @@
 import GithubActionsReporter from "vitest-github-actions-reporter";
 import { defineConfig } from "vitest/config";
 
+const isCI = !!process.env["GITHUB_ACTIONS"];
+
 export default defineConfig({
   test: {
     testTimeout: 30_000,
-    reporters: process.env["GITHUB_ACTIONS"]
-      ? ["default", new GithubActionsReporter()]
-      : "default",
+    reporters: isCI ? ["default", new GithubActionsReporter()] : "default",
 
     coverage: {
       branches: 91,
       functions: 100,
       lines: 95,
       statements: 95,
+      reporter: isCI ? ["lcov", "text"] : ["html", "text"],
     },
   },
 });
