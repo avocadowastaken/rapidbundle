9fa31b34cb6d8eb5a84fa11c6460f9471b39dd9d
avocadowastaken <4734297+avocadowastaken@users.noreply.github.com>
2023-02-18 15:03:02 +0200

    refactor: Simplify task runners. (#106)

--- src/utils/task.ts
+++ src/utils/task.ts
@@ -1,26 +1,5 @@
-import type TaskTree from "tasktree-cli";
 import type { Task } from "tasktree-cli/lib/Task";
-import type { TaskContext } from "../ctx";
 import { ValidationError } from "./validation";
-
-export async function runTaskTree(
-  tree: TaskTree,
-  { noTTY }: TaskContext,
-  fn: () => Promise<void>
-): Promise<void> {
-  tree.start({ silent: noTTY, autoClear: false });
-  try {
-    await fn();
-    tree.stop();
-  } finally {
-    if (noTTY) {
-      console.log(tree.render().join("\n"));
-    }
-  }
-}
-
-export type TaskGenerator<T = void> = AsyncGenerator<string, T>;
-export type TaskProcess<T = void> = Promise<T> | TaskGenerator<T>;
 
 function cleanupErrorMessage(input: string): string {
   return input.replace(/([{}])/g, "\\\\$1");
@@ -48,35 +27,11 @@ function processTaskError(task: Task, error: unknown): never {
   task.fail();
 }
 
-async function processTaskGenerator<T>(
-  task: Task,
-  generator: TaskGenerator<T>
-): Promise<T> {
-  let next: IteratorResult<string, T>;
-  do {
-    next = await generator.next();
-    if (!next.done) {
-      task.log(next.value);
-    }
-  } while (!next.done);
-  return next.value;
-}
-
-export async function runTask<T = void>(
-  task: Task,
-  fn: () => TaskProcess<T>
-): Promise<T> {
+export async function runTask<T>(task: Task, fn: () => Promise<T>): Promise<T> {
   try {
-    const taskProcess = fn();
-    let taskResult: T;
-    if (taskProcess instanceof Promise) {
-      taskResult = await taskProcess;
-    } else {
-      taskResult = await processTaskGenerator(task, taskProcess);
-    }
-
+    const result = await fn();
     task.complete();
-    return taskResult;
+    return result;
   } catch (error) {
     processTaskError(task, error);
   }
