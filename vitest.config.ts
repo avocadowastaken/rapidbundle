86d67ea22c27239fb3958b32ad02a8281c9e42af
avocadowastaken <4734297+avocadowastaken@users.noreply.github.com>
2023-03-28 15:39:38 +0300

    refactor: Deduplicate code in `tsc` utils. (#108)

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,7 +6,7 @@ const isCI = !!process.env["GITHUB_ACTIONS"];
 export default defineConfig({
   test: {
     isolate: true,
-    testTimeout: 30_000,
+    testTimeout: 60_000,
     outputDiffLines: 500,
     outputDiffMaxLines: 500,
     reporters: isCI ? ["default", new GithubActionsReporter()] : "default",
